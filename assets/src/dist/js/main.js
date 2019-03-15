7768028e306b25ce00a83e5a0dba7b331a13246a
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-03-15 17:55:45 +0400

    npm istalled

---
+++ assets/src/dist/js/main.js
@@ -0,0 +1,20 @@
+//ПЛАГИНЫ
+
+
+// Импортируем необходимые js-файлы Bootstrap 4
+
+//= ../../../../node_modules/bootstrap/js/dist/util.js
+//модальное окно
+//= ../../../../node_modules/bootstrap/js/dist/modal.js
+
+// Импортируем телефонную валидацию
+//= ../../../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
+
+// Импортируем fancybox
+//= ../../../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
+
+
+// Импортируем другие js-файлы
+
+//файл с самописными скриптами
+//= my.js
---
+++ assets/src/dist/js/my.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('[type="tel"]').mask("+7 (999) 999-99-99");
+});
+
+
+// Импортируем формы
+//= form/form.js
