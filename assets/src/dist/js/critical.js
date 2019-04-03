447f7461ee473d533da1cbc533fbb346a0ecaf8d
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-04-03 23:08:31 +0300

    add lazy for iframe

--- assets/src/dist/js/critical.js
+++ assets/src/dist/js/critical.js
@@ -1,29 +1,9 @@
 // Импортируем jQuery
 //= ../../../../node_modules/jquery/dist/jquery.js
 
-// функция давление в кеш браузера прелоудера
-function preloadImages(array) {
-    if (!preloadImages.list) {
-        preloadImages.list = [];
-    }
-    var list = preloadImages.list;
-    for (var i = 0; i < array.length; i++) {
-        var img = new Image();
-        img.onload = function() {
-            var index = list.indexOf(this);
-            if (index !== -1) {
-                list.splice(index, 1);
-            }
-        }
-        list.push(img);
-        img.src = array[i];
-    }
-}
-
-preloadImages(["dist/img/preloader.svg"]);
-
 // Импортируем slick-carousel
 //= ../../../../node_modules/slick-carousel/slick/slick.min.js
 
 // Импортируем ленивую загрузку
 //= ../../../../node_modules/lazyloadxt/dist/jquery.lazyloadxt.min.js
+//= ../../../../node_modules/lazyloadxt/dist/jquery.lazyloadxt.extra.js
--- assets/src/dist/js/main.js
+++ assets/src/dist/js/main.js
@@ -1,5 +1,23 @@
 //ПЛАГИНЫ
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
 
+preloadImages(["dist/img/preloader.svg"]);
 
 // Импортируем необходимые js-файлы Bootstrap 4
 
